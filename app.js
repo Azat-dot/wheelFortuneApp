7ba79e2782ab224a4fd81e754ed3fde66f50820d
azat <azatsafaryan@mail.ru>
2023-07-27 12:52:57 +0500

    working animation, gitignore

---
+++ app.js
@@ -0,0 +1,72 @@
+(function() {
+    const wheel = document.querySelector('.wheel-image');
+    const startButton = document.querySelector('.button');
+    const balance = document.querySelector('.balance');
+    const jackpot = document.querySelector('.jackpot');
+    let deg = 0;
+    let optionValue = [
+        "JACKPOT",
+        "250",
+        "400",
+        "10",
+        "100",
+        "150",
+        "200",
+        "750",
+        ]
+  
+    startButton.addEventListener('click', () => {
+      // Disable button during spin
+      startButton.style.pointerEvents = 'none';
+      // Calculate a new rotation between 5000 and 10 000
+      deg = Math.floor(5000 + Math.random() * 5000);
+      // Set the transition on the wheel
+      wheel.style.transition = `all 1s ease-out`;
+      // Rotate the wheel
+      wheel.style.transform = `rotate(${deg}deg)`;
+      // Apply the blur
+      wheel.classList.add('blur');
+    });
+  
+    wheel.addEventListener('transitionend', () => {
+      // Remove blur
+      wheel.classList.remove('blur');
+      // Enable button when spin is over
+      startButton.style.pointerEvents = 'auto';
+      // Need to set transition to none as we want to rotate instantly
+      wheel.style.transition = 'none';
+      // Calculate degree on a 360 degree basis to get the "natural" real rotation
+      // Important because we want to start the next spin from that one
+      // Use modulus to get the rest value from 360
+      const actualDeg = deg % 360;
+      console.log(actualDeg);
+      if (actualDeg <= 50 && actualDeg >= 5) {
+        balance.innerHTML = `BALANCE ${optionValue[7]}`;
+        } else if (
+          actualDeg > 50 &&  actualDeg < 95) {
+        balance.innerHTML = `BALANCE ${optionValue[6]}`;
+        } else if (
+          actualDeg >= 95 && actualDeg < 140) {
+        balance.innerHTML = `BALANCE ${optionValue[5]}`;
+        } else if (
+          actualDeg >= 140 && actualDeg < 185) {
+        balance.innerHTML = `BALANCE  ${optionValue[4]}`;
+        } else if (
+            actualDeg >= 185 && actualDeg < 230) {
+        balance.innerHTML = `BALANCE <br>${optionValue[3]}`;
+        } else if (
+            actualDeg >= 230 && actualDeg < 275) {
+        balance.innerHTML = `BALANCE ${optionValue[2]}`;
+        } else if (
+            actualDeg >= 275 && actualDeg < 320) {
+        balance.innerHTML = `BALANCE ${optionValue[1]}`;
+        } else {
+        balance.innerHTML = `BALANCE ${1000}`
+            jackpot.style.background = "linear-gradient(241.78deg, #BC0000 27.02%, #8B0000 82.05%)",
+            "linear-gradient(180deg, #FFCD7E 0%, #E18A00 100%)";
+        }
+
+      // Set the real rotation instantly without animation
+      wheel.style.transform = `rotate(${actualDeg}deg)`;
+    });
+  })();
