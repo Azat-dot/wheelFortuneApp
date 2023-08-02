776cb8cfdf01ba8cd6e2bfaf744b01ea4bc31aee
azat <azatsafaryan@mail.ru>
2023-08-02 16:35:12 +0500

    fixed logic

--- app.js
+++ app.js
@@ -3,7 +3,17 @@
     const balance = document.querySelector('.balance');
     const jackpot = document.querySelector('.jackpot');
     const moneyTotal = document.querySelector('.money-totall');
+    const backWindow = document.querySelector('.back-window');
+    const greatButton = document.querySelector(".congrat-button")
+    const backWindowBalance = document.querySelector(".back-window_money")
     let deg = 0;
+    let win;
+    let balance_number = 0;
+
+    // (balance_number === undefined)
+    //   ? balance_number = 0
+    //   : balance_number = balance_number;
+
     let optionValue = [
         "JACKPOT",
         "250",
@@ -17,6 +27,7 @@
   
     startButton.addEventListener('click', () => {
       // Disable button during spin
+
       startButton.style.pointerEvents = 'none';
       // Calculate a new rotation between 5000 and 10 000
       deg = Math.floor(5000 + Math.random() * 5000);
@@ -26,11 +37,18 @@
       wheel.style.transform = `rotate(${deg}deg)`;
       // Apply the blur
       wheel.classList.add('blur');
+
+      // let balance_number = balance.innerText.match(/\d{1,}/);
+      // balance_number = balance_number-50;
+      // balance.innerHTML = "BALANCE <br />"+balance_number;
+      setTimeout(()=>backWindow.style.display = 'block', 2000);
     });
   
     wheel.addEventListener('transitionend', () => {
       // Remove blur
       wheel.classList.remove('blur');
+      backWindow.classList.add('block');
+
       // Enable button when spin is over
       startButton.style.pointerEvents = 'auto';
       // Need to set transition to none as we want to rotate instantly
@@ -39,43 +57,46 @@
       // Important because we want to start the next spin from that one
       // Use modulus to get the rest value from 360
       const actualDeg = deg % 360;
-      let win;
+
+      greatButton.addEventListener('click', () => {
+        backWindow.style.display = 'none'
+      })
+
+      balance_number = balance.innerText.match(/\d{1,}/);
 
       switch (true){
         case actualDeg <= 50 && actualDeg >= 5:
-          balance.innerHTML = `BALANCE ${optionValue[7]}`;
           win = +optionValue[7];
           break;
         case actualDeg > 50 &&  actualDeg < 95:
-          balance.innerHTML = `BALANCE ${optionValue[6]}`;
           win = +optionValue[6];
           break;
         case actualDeg >= 95 && actualDeg < 140:
-          balance.innerHTML = `BALANCE ${optionValue[5]}`;
           win = +optionValue[5];
           break;
         case actualDeg >= 140 && actualDeg < 185:
-          balance.innerHTML = `BALANCE ${optionValue[4]}`;
           win = +optionValue[4];
           break;
         case actualDeg >= 185 && actualDeg < 230:
-          balance.innerHTML = `BALANCE ${optionValue[3]}`;
           win = +optionValue[3];
           break;
         case actualDeg >= 230 && actualDeg < 275:
-          balance.innerHTML = `BALANCE ${optionValue[2]}`;
           win = +optionValue[2];
           break;
         case actualDeg >= 275 && actualDeg < 320:
-          balance.innerHTML = `BALANCE ${optionValue[1]}`;
           win = +optionValue[1];
           break;
         default:
-          balance.innerHTML = `BALANCE ${optionValue[7]}`;
           win = +optionValue[7];
     }
 
-        moneyTotal.innerHTML = `${win}`
+        backWindowBalance.innerHTML =  win
+        
+        balance_number = +balance_number +(+parseInt(win))-50;
+      
+        balance.innerHTML = "BALANCE " + balance_number
+
+        moneyTotal.innerHTML = balance_number
 
 
       // Set the real rotation instantly without animation
